0c7cba6898c20040b22d1739512d7ade40abe34c
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-10-05 12:36:22 +0000

    Initial checkin of libfilezilla

---
+++ lib/libfilezilla/libfilezilla.hpp
@@ -0,0 +1,7 @@
+#ifndef LIBFILEZILLA_HEADER
+#define LIBFILEZILLA_HEADER
+
+#include "private/defs.hpp"
+#include "string.hpp"
+
+#endif
---
+++ lib/libfilezilla/private/defs.hpp
@@ -0,0 +1,19 @@
+#ifndef LIBFILEZILLA_CONFIG_HEADER
+#define LIBFILEZILLA_CONFIG_HEADER
+
+// Set some mandatory defines which are used to select platform implementations
+#if !defined(FZ_WINDOWS) && !defined(FZ_MAC) && !defined(FZ_UNIX)
+	#if defined(_WIN32) || defined(_WIN64)
+		#define FZ_WINDOWS 1
+	#elif defined(__APPLE__)
+		#define FZ_MAC 1
+	#else
+		#define FZ_UNIX 1
+	#endif
+#endif
+
+#if defined(BUILDING_LIBFILEZILLA) && defined(HAVE_CONFIG_H)
+#include "config.hpp"
+#endif
+
+#endif
