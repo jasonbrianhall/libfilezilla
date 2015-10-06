bf7ceaa93f13748f5f846d570478cc8b8335d831
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-10-06 11:51:13 +0000

    Fixes to visibility definition to better handle Windows.

--- lib/libfilezilla/private/visibility.hpp
+++ lib/libfilezilla/private/visibility.hpp
@@ -1,16 +1,50 @@
 #ifndef LIBFILEZILLA_PRIVATE_VISIBILITY_HEADER
 #define LIBFILEZILLA_PRIVATE_VISIBILITY_HEADER
 
-#ifdef FZ_WINDOWS
-  #ifdef BUILDING_LIBFILEZILLA
-	#define FZ_PUBLIC_SYMBOL __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+
+// Symbol visibility. There are two main cases: Building libfilezilla and using it
+#ifdef BUILDING_LIBFILEZILLA
+
+  // Two cases when building: Windows, other platform
+  #ifdef FZ_WINDOWS
+ 
+    // Under Windows we can either use Visual Studio or a proper compiler
+    #ifdef (_MSC_VER)
+      #ifdef DLL_EXPORT
+        #define FZ_PUBLIC_SYMBOL __declspec(dllexport)
+      #endif
+    #else
+      #ifdef DLL_EXPORT
+        #define FZ_PUBLIC_SYMBOL __declspec(dllexport)
+      #else
+        #define FZ_PUBLIC_SYMBOL __attribute__((visibility("default")))
+      #endif
+      #define FZ_PRIVATE_SYMBOL __attribute__((visibility("hidden")))
+    #endif
+
   #else
-	#define FZ_PUBLIC_SYMBOL __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+
+    #define FZ_PUBLIC_SYMBOL __attribute__((visibility("default")))
+    #define FZ_PRIVATE_SYMBOL __attribute__((visibility("hidden")))
+
   #endif
-  #define FZ_PRIVATE_SYMBOL
+
 #else
-  #define FZ_PUBLIC_SYMBOL __attribute__((visibility("default")))
-  #define FZ_PRIVATE_SYMBOL __attribute__((visibility("hidden")))
+
+  // Under MSW it makes a difference whether we use a static library or a DLL
+  #ifdef FZ_WINDOWS && defined(FZ_USING_DLL)
+    #define FZ_PUBLIC_SYMBOL __declspec(dllimport)
+  #endif
+
+#endif
+
+
+#ifndef FZ_PUBLIC_SYMBOL
+#define FZ_PUBLIC_SYMBOL
+#endif
+
+#ifndef FZ_PRIVATE_SYMBOL
+#define FZ_PRIVATE_SYMBOL
 #endif
 
 #endif
