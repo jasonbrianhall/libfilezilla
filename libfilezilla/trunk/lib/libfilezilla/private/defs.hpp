913188431983f14540d682690c39688fdf5fdd0b
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-10-30 09:47:07 +0000

    Add #error if not using a C++14 compiler.

--- libfilezilla/trunk/lib/libfilezilla/private/defs.hpp
+++ libfilezilla/trunk/lib/libfilezilla/private/defs.hpp
@@ -1,5 +1,13 @@
-#ifndef LIBFILEZILLA_CONFIG_HEADER
-#define LIBFILEZILLA_CONFIG_HEADER
+#ifndef LIBFILEZILLA_PRIVATE_DEFS_HEADER
+#define LIBFILEZILLA_PRIVATE_DEFS_HEADER
+
+#if defined(__cplusplus) && (__cplusplus < 201400)
+#if defined(_MSC_VER)
+#error You need to use a C++14 compiler, such as Visual Studio 2015 or higher.
+#else
+#error You need to use a C++14 compiler. Try passing -std=c++14 as compiler flag.
+#endif
+#endif
 
 // Set some mandatory defines which are used to select platform implementations
 #if !defined(FZ_WINDOWS) && !defined(FZ_MAC) && !defined(FZ_UNIX)
