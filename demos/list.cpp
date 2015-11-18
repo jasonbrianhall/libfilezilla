adb044e7e30526cb6a85fa0d1735408c7571d62f
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-11-18 15:22:20 +0000

    Add demo program for local_filesys

---
+++ demos/list.cpp
@@ -0,0 +1,50 @@
+#include <libfilezilla/local_filesys.hpp>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	fz::native_string path = fzT(".");
+
+	if (argc > 1 && argv[1] && *argv[1]) {
+		path = fz::to_native(std::string(argv[1]));
+	}
+
+	fz::local_filesys fs;
+
+	// Begin listing
+	if (!fs.begin_find_files(path)) {
+		std::cerr << "Cannot list " << path << std::endl;
+		return 1;
+	}
+
+	std::cout << "Listing " << path << "\n\n";
+	std::cout << "Name Link? Type Size Modified Mode\n";
+	std::cout << "----------------------------------\n";
+
+	fz::native_string name;
+	int64_t size;
+	fz::datetime time;
+	bool is_link;
+	bool is_dir;
+	int mode;
+
+	// Iterate over it
+	while (fs.get_next_file(name, is_link, is_dir, &size, &time, &mode)) {
+
+		// Print results
+		std::cout << name << "\n";
+		std::cout << "    Type: " << (is_link ? "symlinked" : "regular") << (is_dir ? " directory" : " file") << "\n";
+		if (!is_dir) {
+			if (size >= 0) {
+				std::cout << "    Size: " << size << " octets" << "\n";
+			}
+		}
+		if (!time.empty()) {
+			std::cout << "    Last modified: " << time.format("%Y-%m-%d %H-%M-%S ", fz::datetime::local) << "\n";
+		}
+		std::cout << "    Mode: " << mode << "\n" << std::endl;
+	}
+
+	return 0;
+}
